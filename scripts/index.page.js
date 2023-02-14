0bdf256f1f35589a9216be13974b8bca7749292c
Michael Deng <michaeldeng55@gmail.com>
2023-02-14 11:05:53 -0500

    comment display almost done

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -1,18 +1,18 @@
 const commentLog = [
     {
-        profile: "",
+        src: "../assets/images/Mohan-muruge.jpg",
         name: "Connor Walton",
         date: "02/17/2021",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     },
     {
-        profile: "",
+        src: null,
         name: "Emilie Beach",
         date: "01/09/2021",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
-        profile: "",
+        src: null,
         name: "Miles Acosta",
         date: "12/20/2020",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
@@ -21,7 +21,6 @@ const commentLog = [
 ]
 
 const commentContainer = document.querySelector('.comments__container');
-console.log(commentContainer);
 //when the comments button is clicked, evoke the function again
 
 //on page load and on comment submission, this function should be evoked so that the page gets re-rendered. 
@@ -37,10 +36,14 @@ function generateCommentHTML(com) {
     //left side, picture
     const leftSection = document.createElement('div');
     leftSection.className = "comments__container__block__profile";
+    const profileBlock = document.createElement('div');
+    profileBlock.className = "comments__container__block__profile__icon"
     const profile = document.createElement('img');
     profile.className = "comments__container__block__profile__picture";
-    profile.src = com.profile;
-    leftSection.appendChild(profile);
+    profile.src = setImage(com);
+    profile.alt = "picture"
+    profileBlock.appendChild(profile)
+    leftSection.appendChild(profileBlock);
     container.appendChild(leftSection);
     //right side, text
     const rightSection = document.createElement('div');
@@ -49,11 +52,13 @@ function generateCommentHTML(com) {
     const rightSectionTop = document.createElement('div');
     rightSectionTop.className = "comments__container__block__text__top";
     const name = document.createElement('p');
-    name.className = "comments__container__block__text__top__name";
+    name.className = "comments__container__block__text__top__name bold";
+    name.style.color = "#323232"
     name.innerText = com.name;
     const date = document.createElement('p');
     date.className = "comments__container__block__text__top__date";
     date.innerText = com.date;
+    date.style.color = "#AFAFAF"
     rightSectionTop.appendChild(name);
     rightSectionTop.appendChild(date);
     rightSection.appendChild(rightSectionTop)
@@ -66,4 +71,13 @@ function generateCommentHTML(com) {
     return container;
 }
 
+function setImage(object) {
+    if (!object.src) {
+        return null;
+    }else {
+        return object.src
+    }
+}
+
 displayComment();
+
